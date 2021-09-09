d634fdec4886a3402792cdaa049162caa4788699
Tiagodev <tiagodev@protonmail.com>
2021-09-09 12:13:50 -0700

    implementing useReducer

--- src/components/ReducerHook/ReducerHook.styles.js
+++ src/components/ReducerHook/ReducerHook.styles.js
@@ -1,3 +1,23 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 export const Wrapper = styled.div``;
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: whitesmoke;
+`;
+
+export const Title = styled.h1`
+  padding: 10px;
+  font-size: 40px;
+`;
+
+export const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  margin: 10px;
+`;
--- src/components/ReducerHook/index.js
+++ src/components/ReducerHook/index.js
@@ -1,9 +1,24 @@
-import { Wrapper } from "./ReducerHook.styles";
+import { useReducer } from "react";
+import { Button, Container, Title, Wrapper } from "./ReducerHook.styles";
+
+function reducer(state, action) {
+  if (action === "increment") return state + 1;
+  if (action === "decrement") return state - 1;
+  if (action === "reset") return 0;
+  throw new Error();
+}
 
 export default function ReducerHook() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <Wrapper>
-      <p>ReducerHook</p>
+      <Container>
+        <Title>Count: {count}</Title>
+        <Button onClick={() => dispatch("increment")}>+</Button>
+        <Button onClick={() => dispatch("decrement")}>-</Button>
+        <Button onClick={() => dispatch("reset")}>reset</Button>
+      </Container>
     </Wrapper>
   );
 }
